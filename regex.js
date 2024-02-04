c8bf46826af1ecce0b1cf212e88b3dc8c70c7653
emon9X <shahariar.emn990@gmail.com>
2024-02-04 12:41:20 +0600

    basics

---
+++ regex.js
@@ -0,0 +1,12 @@
+//creating regex
+
+{
+    let regexLiteral = /pattern/;
+    console.log(regexLiteral);
+    let regexConstructor = new RegExp('pattern');
+    console.log(regexConstructor);
+}
+
+//syntax
+//---> /pattern/modifiers
+//text.method(/pattern/modifiers)
